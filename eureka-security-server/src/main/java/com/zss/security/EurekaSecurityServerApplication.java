6e5ee6dd509fbdbf9984984145ef70054f0039ec
张富国、 <fuguo.zhang@jyblife.com>
2019-12-25 18:46:22 +0800

    feat:创建maven项目

---
+++ eureka-security-server/src/main/java/com/zss/security/EurekaSecurityServerApplication.java
@@ -0,0 +1,16 @@
+package com.zss.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fuguozhang
+ * @email fuguozhang@jyblife.com
+ * @date 2019/12/25 17:51
+ */
+@SpringBootApplication
+public class EurekaSecurityServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaSecurityServerApplication.class,args);
+    }
+}
