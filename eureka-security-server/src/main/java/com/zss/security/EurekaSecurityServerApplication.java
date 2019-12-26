fb8c9088bb8acd7bbdbb248275180266b3ba1087
张富国、 <fuguo.zhang@jyblife.com>
2019-12-26 11:47:03 +0800

    feat:添加注册中心认证服务

--- eureka-security-server/src/main/java/com/zss/security/EurekaSecurityServerApplication.java
+++ eureka-security-server/src/main/java/com/zss/security/EurekaSecurityServerApplication.java
@@ -2,12 +2,14 @@ package com.zss.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author fuguozhang
  * @email fuguozhang@jyblife.com
  * @date 2019/12/25 17:51
  */
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaSecurityServerApplication {
     public static void main(String[] args) {
--- eureka-security-server/src/main/java/com/zss/security/config/WebSecurityConfig.java
+++ eureka-security-server/src/main/java/com/zss/security/config/WebSecurityConfig.java
@@ -13,7 +13,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().ignoringAntMatchers("/eureka/**","/**");
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        //http.csrf().disable();
         super.configure(http);
     }
 }
